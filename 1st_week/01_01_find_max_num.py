cdad1e902ed6bedd88d0b2d367d6b24cbc4d6174
edohan <edohan0117@gmail.com>
2025-04-28 13:30:18 +0900

    25.04.28

---
+++ 1st_week/01_01_find_max_num.py
@@ -0,0 +1,34 @@
+def find_max_num(array):
+    for number in array:
+        is_max_num = True
+        for compare_num in array:
+            if number < compare_num:
+                is_max_num = False
+        if is_max_num:
+            return  number
+
+
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
+print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
+
+def find_max_num2(array):
+    max_num = array[0]
+    for num in array:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+
+print("정답 = 6 / 현재 풀이 값2 = ", find_max_num2([3, 5, 6, 1, 2, 4]))
+print("정답 = 6 / 현재 풀이 값2 = ", find_max_num2([6, 6, 6]))
+print("정답 = 1888 / 현재 풀이 값2 = ", find_max_num2([6, 9, 2, 7, 1888]))
+
+def find_max_num3(array):
+    
+    return max(array)
+
+
+print("정답 = 6 / 현재 풀이 값3 = ", find_max_num3([3, 5, 6, 1, 2, 4]))
+print("정답 = 6 / 현재 풀이 값3 = ", find_max_num3([6, 6, 6]))
+print("정답 = 1888 / 현재 풀이 값3 = ", find_max_num3([6, 9, 2, 7, 1888]))
