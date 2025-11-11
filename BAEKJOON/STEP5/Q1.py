7de45e35163ca6a4288d538bab86ab70c8e5cdc8
jaekeun <baik117@hkcloud.co.kr>
2025-11-11 18:26:35 +0900

    2025.11.11

--- BAEKJOON/STEP5/Q1.py
+++ BAEKJOON/STEP5/Q1.py
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 print("         ,r'\"7")
 print("r`-_   ,'  ,/")
 print(" \\. \". L_r'")
 print("   `~\\/")
 print("      |")
 print("      |")
-=======
 S = input()
 i = int(input())
 
 print(S[i-1])
->>>>>>> 7ef7aefa455e6a4bac4e98e1b93fc102a692eec3
--- BAEKJOON/STEP5/Q2.py
+++ BAEKJOON/STEP5/Q2.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 piece = list(map(int, input().split()))
 standard = [1, 1, 2, 2, 2, 8]
 result = [s - p for s, p in zip(standard, piece)]
 print(*result)
-=======
 S = input()
 print(len(S))
->>>>>>> 7ef7aefa455e6a4bac4e98e1b93fc102a692eec3
